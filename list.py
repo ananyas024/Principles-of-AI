8d4053631028c58bb98ba4bfbee154ee36736ff9
ananyas024 <ananyakolathaya@gmail.com>
2024-05-13 21:55:09 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,19 @@
+
+fruits = ["apple", "banana"]
+print("Initial list:", fruits)
+
+
+fruits.append("orange")  
+print("After appending 'orange':", fruits)
+
+
+fruits.insert(1, "grape") 
+print("After inserting 'grape' at index 1:", fruits)
+
+
+fruits.remove("apple") 
+print("After removing 'apple':", fruits)
+
+
+del fruits[0] 
+print("After deleting the element at index 0:", fruits)
